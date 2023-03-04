fa06c3579fafe3f3b83e2038264236857bd3d1f8
Steven Penny <rqwwof@riseup.net>
2023-03-04 00:10:49 -0600

    +   "flag"

--- cmd/riseup-alias/riseup-alias.go
+++ cmd/riseup-alias/riseup-alias.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+   "flag"
    "fmt"
    "strconv"
    "time"
 )
 
 func main() {
-   // wikipedia.org/wiki/GitHub
-   then := time.Date(2008, 2, 8, 0, 0, 0, 0, time.UTC)
-   dur := time.Now().Sub(then).Seconds()
-   id := strconv.FormatInt(int64(dur), 36)
-   fmt.Println(id + "@riseup.net")
+   var year int
+   flag.IntVar(&year, "y", 0, "year")
+   flag.Parse()
+   if year >= 1 {
+      then := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+      { // seconds
+         dur := time.Now().Sub(then).Seconds()
+         id := strconv.FormatInt(int64(dur), 36)
+         fmt.Println(id + "@riseup.net")
+      }
+      { // minutes
+         dur := time.Now().Sub(then).Minutes()
+         id := strconv.FormatInt(int64(dur), 36)
+         fmt.Println(id + "@riseup.net")
+      }
+      { // hours
+         dur := time.Now().Sub(then).Hours()
+         id := strconv.FormatInt(int64(dur), 36)
+         fmt.Println(id + "@riseup.net")
+      }
+   } else {
+      flag.Usage()
+   }
 }
