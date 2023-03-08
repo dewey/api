0db3b1c4cc9f7546a0d5df42363ffc90cb645d87
Steven Penny <rqwwof@riseup.net>
2023-03-08 16:09:42 -0600

    new file:   blog/itoa.go

---
+++ blog/itoa.go
@@ -0,0 +1,114 @@
+func formatBits(dst []byte, u uint64, base int, neg, append_ bool) (d []byte, s string) {
+	if base < 2 || base > len(digits) {
+		panic("strconv: illegal AppendInt/FormatInt base")
+	}
+	// 2 <= base && base <= len(digits)
+
+	var a [64 + 1]byte // +1 for sign of 64bit value in base 2
+	i := len(a)
+
+	if neg {
+		u = -u
+	}
+
+	// convert bits
+	// We use uint values where we can because those will
+	// fit into a single register even on a 32bit machine.
+	if base == 10 {
+		// common case: use constants for / because
+		// the compiler can optimize it into a multiply+shift
+
+		if host32bit {
+			// convert the lower digits using 32bit operations
+			for u >= 1e9 {
+				// Avoid using r = a%b in addition to q = a/b
+				// since 64bit division and modulo operations
+				// are calculated by runtime functions on 32bit machines.
+				q := u / 1e9
+				us := uint(u - q*1e9) // u % 1e9 fits into a uint
+				for j := 4; j > 0; j-- {
+					is := us % 100 * 2
+					us /= 100
+					i -= 2
+					a[i+1] = smallsString[is+1]
+					a[i+0] = smallsString[is+0]
+				}
+
+				// us < 10, since it contains the last digit
+				// from the initial 9-digit us.
+				i--
+				a[i] = smallsString[us*2+1]
+
+				u = q
+			}
+			// u < 1e9
+		}
+
+		// u guaranteed to fit into a uint
+		us := uint(u)
+		for us >= 100 {
+			is := us % 100 * 2
+			us /= 100
+			i -= 2
+			a[i+1] = smallsString[is+1]
+			a[i+0] = smallsString[is+0]
+		}
+
+		// us < 100
+		is := us * 2
+		i--
+		a[i] = smallsString[is+1]
+		if us >= 10 {
+			i--
+			a[i] = smallsString[is]
+		}
+
+	} else if isPowerOfTwo(base) {
+		// Use shifts and masks instead of / and %.
+		// Base is a power of 2 and 2 <= base <= len(digits) where len(digits) is 36.
+		// The largest power of 2 below or equal to 36 is 32, which is 1 << 5;
+		// i.e., the largest possible shift count is 5. By &-ind that value with
+		// the constant 7 we tell the compiler that the shift count is always
+		// less than 8 which is smaller than any register width. This allows
+		// the compiler to generate better code for the shift operation.
+		shift := uint(bits.TrailingZeros(uint(base))) & 7
+		b := uint64(base)
+		m := uint(base) - 1 // == 1<<shift - 1
+		for u >= b {
+			i--
+			a[i] = digits[uint(u)&m]
+			u >>= shift
+		}
+		// u < base
+		i--
+		a[i] = digits[uint(u)]
+	} else {
+		// general case
+		b := uint64(base)
+		for u >= b {
+			i--
+			// Avoid using r = a%b in addition to q = a/b
+			// since 64bit division and modulo operations
+			// are calculated by runtime functions on 32bit machines.
+			q := u / b
+			a[i] = digits[uint(u-q*b)]
+			u = q
+		}
+		// u < base
+		i--
+		a[i] = digits[uint(u)]
+	}
+
+	// add sign, if any
+	if neg {
+		i--
+		a[i] = '-'
+	}
+
+	if append_ {
+		d = append(dst, a[i:]...)
+		return
+	}
+	s = string(a[i:])
+	return
+}
---
+++ blog/readme.md
@@ -0,0 +1,3 @@
+# Blog
+
+https://github.com/golang/go/blob/go1.20.2/src/strconv/itoa.go#L88-L201
