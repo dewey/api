a7df9ef2c3d7138946f74983188a7600ed24b43b
Steven Penny <rqwwof@riseup.net>
2023-03-31 06:05:48 -0500

    -   http.Client

--- cmd/justwatch/main.go
+++ cmd/justwatch/main.go
@@ -8,7 +8,6 @@ import (
 
 type flags struct {
    address string
-   http.Client
    language string
    sleep time.Duration
 }
@@ -17,7 +16,7 @@ func main() {
    var f flags
    flag.StringVar(&f.address, "a", "", "address")
    flag.StringVar(&f.language, "lang", "en", "language")
-   flag.IntVar(&f.Log_Level, "log", 0, "log level")
+   flag.IntVar(&http.Default_Client.Log_Level, "log", 0, "log level")
    flag.DurationVar(&f.sleep, "s", 99*time.Millisecond, "sleep")
    flag.Parse()
    if f.address != "" {
