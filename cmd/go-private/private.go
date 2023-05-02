11655c6e261cd7841b7679ddb2c234451f726600
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-02 05:10:39 -0500

    +      cmd := exec.Command("gofmt", "-w", "-r", before+" -> "+after, ".")

--- cmd/go-private/private.go
+++ cmd/go-private/private.go
@@ -3,11 +3,23 @@ package main
 import (
    "fmt"
    "os"
+   "os/exec"
+   "strings"
 )
 
 func main() {
    if len(os.Args) == 2 {
+      before := os.Args[1]
+      after := strings.ToLower(before[:1]) + before[1:]
+      cmd := exec.Command("gofmt", "-w", "-r", before+" -> "+after, ".")
+      cmd.Stdout = os.Stdout
+      cmd.Stderr = os.Stderr
+      fmt.Printf("%q\n", cmd.Args)
+      err := cmd.Run()
+      if err != nil {
+         panic(err)
+      }
    } else {
-      fmt.Println("private [identifier]")
+      fmt.Println("go-private [identifier]")
    }
 }
