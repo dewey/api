7076ff8fd4e76627e298c157a7a5caac70aee299
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-10 17:41:50 -0500

    new file:   cmd/git-askpass/askpass.go

--- cmd/git-askpass/main.go
+++ cmd/git-askpass/main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-   "bufio"
    "fmt"
-   "net/url"
    "os"
    "path/filepath"
    "strings"
 )
-
-func credentials(name string) ([]url.URL, error) {
-   file, err := os.Open(name)
-   if err != nil {
-      return nil, err
-   }
-   defer file.Close()
-   var refs []url.URL
-   buf := bufio.NewScanner(file)
-   for buf.Scan() {
-      var ref url.URL
-      err := ref.UnmarshalBinary(buf.Bytes())
-      if err != nil {
-         return nil, err
-      }
-      refs = append(refs, ref)
-   }
-   return refs, nil
-}
 
 func main() {
    home, err := os.UserHomeDir()
