73c91c39a75a7c362e90e921c24cfa92d112f220
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-12 17:49:53 -0500

    +   data, err := os.ReadFile("_gvimrc")

--- cmd/install-gvim/gvimrc.go
+++ cmd/install-gvim/gvimrc.go
@@ -3,18 +3,9 @@ package main
 import "os"
 
 func do_gvimrc(home string) error {
-   in, err := os.Open("_gvimrc")
+   data, err := os.ReadFile("_gvimrc")
    if err != nil {
       return err
    }
-   defer in.Close()
-   out, err := os.Create(home + "/_gvimrc")
-   if err != nil {
-      return err
-   }
-   defer out.Close()
-   if _, err := out.ReadFrom(in); err != nil {
-      return err
-   }
-   return nil
+   return os.WriteFile(home + "/_gvimrc", data, os.ModePerm)
 }
