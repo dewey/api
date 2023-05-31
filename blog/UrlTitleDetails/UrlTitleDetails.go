2174b7a686e65eb624b2b302108a4ed1f06a8f7f
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-30 21:14:25 -0500

    -            __typename

--- blog/UrlTitleDetails/UrlTitleDetails.go
+++ blog/UrlTitleDetails/UrlTitleDetails.go
@@ -70,13 +70,10 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
           package {
             id
             packageId
-            __typename
           }
-          __typename
         }
         promotedBundles(country: $country, platform: $platform) {
           promotionUrl
-          __typename
         }
         availableTo(country: $country, platform: $platform) {
           availableCountDown(country: $country)
@@ -84,55 +81,43 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
           package {
             id
             shortName
-            __typename
           }
-          __typename
         }
         fallBackClips: content(country: "US", language: "en") {
           videobusterClips: clips(providers: [VIDEOBUSTER]) {
             externalId
             provider
-            __typename
           }
           dailymotionClips: clips(providers: [DAILYMOTION]) {
             externalId
             provider
-            __typename
           }
-          __typename
         }
         content(country: $country, language: $language) {
           backdrops {
             backdropUrl
-            __typename
           }
           clips {
             externalId
             provider
-            __typename
           }
           videobusterClips: clips(providers: [VIDEOBUSTER]) {
             externalId
             provider
-            __typename
           }
           dailymotionClips: clips(providers: [DAILYMOTION]) {
             externalId
             provider
-            __typename
           }
           videobusterClips: clips(providers: [VIDEOBUSTER]) {
             externalId
-            __typename
           }
           externalIds {
             imdbId
-            __typename
           }
           fullPath
           genres {
             shortName
-            __typename
           }
           posterUrl
           runtime
@@ -142,7 +127,6 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
             imdbVotes
             tmdbPopularity
             tmdbScore
-            __typename
           }
           shortDescription
           title
@@ -152,65 +136,39 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
             releaseCountDown(country: $country)
             releaseDate
             label
-            package {
-              id
-              packageId
-              shortName
-              __typename
-            }
-            __typename
           }
           ... on MovieOrShowContent {
             originalTitle
             ageCertification
-            credits {
-              role
-              name
-              characterName
-              personId
-              __typename
-            }
             productionCountries
-            __typename
           }
           ... on SeasonContent {
             seasonNumber
-            __typename
           }
-          __typename
         }
-        __typename
       }
       ... on MovieOrShow {
         watchlistEntry {
           createdAt
-          __typename
         }
         likelistEntry {
           createdAt
-          __typename
         }
         dislikelistEntry {
           createdAt
-          __typename
         }
         customlistEntries {
           createdAt
           genericTitleList {
             id
-            __typename
           }
-          __typename
         }
-        __typename
       }
       ... on Movie {
         permanentAudiences
         seenlistEntry {
           createdAt
-          __typename
         }
-        __typename
       }
       ... on Show {
         permanentAudiences
@@ -218,7 +176,6 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
         seenState(country: $country) {
           progress
           seenEpisodeCount
-          __typename
         }
         seasons(sortDirection: DESC) {
           id
@@ -227,45 +184,18 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
           availableTo(country: $country, platform: $platform) {
             availableToDate
             availableCountDown(country: $country)
-            package {
-              id
-              shortName
-              __typename
-            }
-            __typename
           }
           content(country: $country, language: $language) {
             posterUrl
             seasonNumber
             fullPath
-            upcomingReleases(releaseTypes: DIGITAL) {
-              releaseDate
-              releaseCountDown(country: $country)
-              package {
-                id
-                shortName
-                __typename
-              }
-              __typename
-            }
             isReleased
-            __typename
           }
           show {
             id
             objectId
             objectType
-            watchlistEntry {
-              createdAt
-              __typename
-            }
-            content(country: $country, language: $language) {
-              title
-              __typename
-            }
-            __typename
           }
-          __typename
         }
         recentEpisodes: episodes(
           sortDirection: DESC
@@ -280,20 +210,11 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
             episodeNumber
             seasonNumber
             isReleased
-            upcomingReleases {
-              releaseDate
-              label
-              __typename
-            }
-            __typename
           }
           seenlistEntry {
             createdAt
-            __typename
           }
-          __typename
         }
-        __typename
       }
       ... on Season {
         totalEpisodeCount
@@ -303,7 +224,6 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
           objectId
           seenlistEntry {
             createdAt
-            __typename
           }
           content(country: $country, language: $language) {
             title
@@ -311,19 +231,7 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
             episodeNumber
             seasonNumber
             isReleased
-            upcomingReleases(releaseTypes: DIGITAL) {
-              releaseDate
-              label
-              package {
-                id
-                packageId
-                __typename
-              }
-              __typename
-            }
-            __typename
           }
-          __typename
         }
         show {
           id
@@ -334,22 +242,17 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
             createdAt
             genericTitleList {
               id
-              __typename
             }
-            __typename
           }
           fallBackClips: content(country: "US", language: "en") {
             videobusterClips: clips(providers: [VIDEOBUSTER]) {
               externalId
               provider
-              __typename
             }
             dailymotionClips: clips(providers: [DAILYMOTION]) {
               externalId
               provider
-              __typename
             }
-            __typename
           }
           content(country: $country, language: $language) {
             title
@@ -360,62 +263,38 @@ query GetUrlTitleDetails($fullPath: String!, $country: Country!, $language: Lang
               name
               characterName
               personId
-              __typename
             }
             productionCountries
             externalIds {
               imdbId
-              __typename
             }
             upcomingReleases(releaseTypes: DIGITAL) {
               releaseDate
-              __typename
             }
             backdrops {
               backdropUrl
-              __typename
             }
             posterUrl
             isReleased
-            videobusterClips: clips(providers: [VIDEOBUSTER]) {
-              externalId
-              provider
-              __typename
-            }
-            dailymotionClips: clips(providers: [DAILYMOTION]) {
-              externalId
-              provider
-              __typename
-            }
-            __typename
           }
           seenState(country: $country) {
             progress
-            __typename
           }
           watchlistEntry {
             createdAt
-            __typename
           }
           dislikelistEntry {
             createdAt
-            __typename
           }
           likelistEntry {
             createdAt
-            __typename
           }
-          __typename
         }
         seenState(country: $country) {
           progress
-          __typename
         }
-        __typename
       }
-      __typename
     }
-    __typename
   }
 }
 `
