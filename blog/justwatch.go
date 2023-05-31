4e6b1ace0f82f888b82c21514780508bf2a67764
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-30 19:50:19 -0500

    new file:   blog/GetTitleOffers.json

--- blog/justwatch.go
+++ blog/justwatch.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+   "bytes"
    "io"
    "net/http"
-   "net/http/httputil"
    "net/url"
-   "os"
    "strings"
+   "fmt"
 )
+
+const query = `
+query GetTitleOffers(
+   $country: Country!
+   $nodeId: ID!
+   $platform: Platform! = WEB
+) {
+   node(id: $nodeId) {
+      ... on MovieOrShowOrSeasonOrEpisode {
+         offers(
+            country: $country
+            platform: $platform
+         ) {
+            ... on Offer {
+               standardWebURL
+            }
+         }
+      }
+   }
+}
+`
 
 func main() {
    var req http.Request
@@ -17,57 +38,48 @@ func main() {
    req.URL.Host = "apis.justwatch.com"
    req.URL.Path = "/graphql"
    req.URL.Scheme = "https"
-   req.Body = io.NopCloser(req_body)
    req.Header["Content-Type"] = []string{"application/json"}
+   req_body := fmt.Sprintf(`
+{
+   "query": %q,
+   "variables": {
+      "nodeId": "tse371404",
+      "country": "US",
+      "filterFlatrate": {
+         "monetizationTypes": [
+            "FLATRATE",
+            "FLATRATE_AND_BUY",
+            "ADS",
+            "FREE",
+            "CINEMA"
+         ],
+         "bestOnly": true
+      },
+      "filterFree": {
+         "monetizationTypes": [
+            "ADS",
+            "FREE"
+         ],
+         "bestOnly": true
+      }
+   }
+}
+`, query)
+   req.Body = io.NopCloser(strings.NewReader(req_body))
    res, err := new(http.Transport).RoundTrip(&req)
    if err != nil {
       panic(err)
    }
    defer res.Body.Close()
-   res_body, err := httputil.DumpResponse(res, true)
+   res_body, err := io.ReadAll(res.Body)
    if err != nil {
       panic(err)
    }
-   os.Stdout.Write(res_body)
+   fmt.Println(string(res_body))
+   // amcplus.com/shows/orphan-black/episodes/season-1-instinct--1011152
+   if bytes.Contains(res_body, []byte("/season-1-instinct--1011152")) {
+      fmt.Println("pass")
+   } else {
+      fmt.Println("fail")
+   }
 }
-
-var req_body = strings.NewReader(`{
-  "operationName": "GetTitleOffers",
-  "variables": {
-    "platform": "WEB",
-    "nodeId": "tse371404",
-    "country": "US",
-    "language": "en",
-    "filterBuy": {
-      "monetizationTypes": [
-        "BUY"
-      ],
-      "bestOnly": true
-    },
-    "filterFlatrate": {
-      "monetizationTypes": [
-        "FLATRATE",
-        "FLATRATE_AND_BUY",
-        "ADS",
-        "FREE",
-        "CINEMA"
-      ],
-      "bestOnly": true
-    },
-    "filterRent": {
-      "monetizationTypes": [
-        "RENT"
-      ],
-      "bestOnly": true
-    },
-    "filterFree": {
-      "monetizationTypes": [
-        "ADS",
-        "FREE"
-      ],
-      "bestOnly": true
-    }
-  },
-  "query": "query GetTitleOffers($nodeId: ID!, $country: Country!, $language: Language!, $filterFlatrate: OfferFilter!, $filterBuy: OfferFilter!, $filterRent: OfferFilter!, $filterFree: OfferFilter!, $platform: Platform! = WEB) {\n  node(id: $nodeId) {\n    id\n    __typename\n    ... on MovieOrShowOrSeasonOrEpisode {\n      offerCount(country: $country, platform: $platform)\n      flatrate: offers(\n        country: $country\n        platform: $platform\n        filter: $filterFlatrate\n      ) {\n        ...TitleOffer\n        __typename\n      }\n      buy: offers(country: $country, platform: $platform, filter: $filterBuy) {\n        ...TitleOffer\n        __typename\n      }\n      rent: offers(country: $country, platform: $platform, filter: $filterRent) {\n        ...TitleOffer\n        __typename\n      }\n      free: offers(country: $country, platform: $platform, filter: $filterFree) {\n        ...TitleOffer\n        __typename\n      }\n      __typename\n    }\n  }\n}\n\nfragment TitleOffer on Offer {\n  id\n  presentationType\n  monetizationType\n  retailPrice(language: $language)\n  retailPriceValue\n  currency\n  lastChangeRetailPriceValue\n  type\n  package {\n    id\n    packageId\n    clearName\n    __typename\n  }\n  standardWebURL\n  elementCount\n  availableTo\n  deeplinkRoku: deeplinkURL(platform: ROKU_OS)\n  __typename\n}\n"
-}
-`)
