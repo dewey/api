dcf946f6a8fdd3db9412c21cd94a55599462bedf
Steven Penny <srpen6+4cq2@gmail.com>
2023-06-08 06:06:37 -0500

    deleted:    blog/2023-05/go-private/write.go

---
+++ blog/2023-06/go-private/readme.md
@@ -0,0 +1,8 @@
+# exports
+
+~~~
+gofmt -w -r 'Println -> _Println' hello.go
+gofmt -w -r 'a._Println -> a.Println' hello.go
+~~~
+
+<https://wikipedia.org/wiki/Schwartzian_transform>
---
+++ blog/2023-06/go-private/write.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+   "fmt"
+   "os"
+   "os/exec"
+)
+
+func run(name string, arg ...string) error {
+   cmd := exec.Command(name, arg...)
+   cmd.Stdout = os.Stdout
+   cmd.Stderr = os.Stderr
+   fmt.Printf("%q\n", cmd.Args)
+   return cmd.Run()
+}
+
+func (f flags) write() error {
+   err := run("gofmt", "-w", "-r", f.w_before+" -> _"+f.w_before, ".")
+   if err != nil {
+      return err
+   }
+   return run("gofmt", "-w", "-r", "a._"+f.w_before+" -> a."+f.w_before, ".")
+}
