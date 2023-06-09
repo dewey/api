a4a6386425eea71de5b9cc7b830610b4c6b8871f
Steven Penny <srpen6+4cq2@gmail.com>
2023-06-09 05:06:32 -0500

    +   w_module string

--- blog/2023-06/go-private/main.go
+++ blog/2023-06/go-private/main.go
@@ -5,11 +5,13 @@ import "flag"
 type flags struct {
    r_path string
    w_before string
+   w_module string
 }
 
 func main() {
    var f flags
    flag.StringVar(&f.r_path, "r", "", "read path")
+   flag.StringVar(&f.w_module, "m", "2a.pages.dev/tls", "write module")
    flag.StringVar(&f.w_before, "w", "", "write before")
    flag.Parse()
    if f.r_path != "" {
--- blog/2023-06/go-private/write.go
+++ blog/2023-06/go-private/write.go
@@ -4,20 +4,24 @@ import (
    "fmt"
    "os"
    "os/exec"
+   "strings"
 )
 
-func run(name string, arg ...string) error {
-   cmd := exec.Command(name, arg...)
+func (f flags) write() error {
+   var after string
+   i := strings.LastIndexAny(f.w_before, ".:")
+   if i >= 0 {
+      after = f.w_before[i+1:]
+   } else {
+      after = f.w_before
+   }
+   cmd := exec.Command(
+      "gorename",
+      "-from", fmt.Sprintf(`%q%v`, f.w_module, f.w_before),
+      "-to", "_" + after,
+   )
    cmd.Stdout = os.Stdout
    cmd.Stderr = os.Stderr
    fmt.Printf("%q\n", cmd.Args)
    return cmd.Run()
-}
-
-func (f flags) write() error {
-   err := run("gofmt", "-w", "-r", f.w_before+" -> _"+f.w_before, ".")
-   if err != nil {
-      return err
-   }
-   return run("gofmt", "-w", "-r", "net._"+f.w_before+" -> net."+f.w_before, ".")
 }
